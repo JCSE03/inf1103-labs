af4ce6a9c76671cb52ec787b5b869ad7aee3c79f
Johanan 2603048 <2603048@sit.singaporetech.edu.sg>
2026-09-18 14:45:31 +0800

    added get_valid_input() function, week 3 requirement 1

--- week 3/modular_auditor.py
+++ week 3/modular_auditor.py
@@ -1,26 +1,36 @@
+def get_valid_inputs():
+    global failed_entries
+    while True:
+        userInput = input("Enter a stock quantity: (or type 'quit' to quit)")
+        if userInput == "quit":
+            return "quit"
+        
+        elif userInput.startswith("-") and userInput[1:].isdigit():
+            print("Error: Negative numbers are not allowed.")
+            failed_entries += 1
+
+        elif userInput.isdigit():
+            return int(userInput)
+        
+        else:
+            print("Error: Invalid entry. Please enter a valid number.")
+            failed_entries += 1
+
 inventory = 0
 failed_entries = 0  # Requirement 8: Track rejected inputs
 
 while True:
-    userInput = input("Enter a stock quantity: (or type 'quit' to quit)")
+    val = get_valid_inputs()
 
-    if userInput == "quit":
+    if val == "quit":
         break
 
-    elif userInput.startswith("-") and userInput[1:].isdigit():
-        print("Error: Negative numbers are not allowed.")
-        failed_entries += 1 
+    inventory += val
 
-    elif userInput.isdigit():
-        inventory += int(userInput)
+    if inventory > 500:
+        print("OVERSTOCK ALERT: Total inventory of " + str(inventory) + " exceeds limit of 500 units!")
+        break
 
-        if inventory > 500:
-            print("OVERSTOCK ALERT: Total inventory of "  + str(inventory) + " exceeds limit of 500 units!")
-            break
-
-    else:
-        print("Error: Invalid entry. Please enter a valid number.")
-        failed_entries += 1  # Track non-digit entry rejection
 
 print("Total Units Processed: " + str(inventory))
 print("Number of Failed/Rejected Entries: "  +str(failed_entries))
