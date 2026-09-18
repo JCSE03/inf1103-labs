8034c86c28bfb54217953926ebf6d371a4637a3b
Johanan 2603048 <2603048@sit.singaporetech.edu.sg>
2026-09-18 14:49:10 +0800

    added process_delivery func, week 3 requirement 2

--- week 3/modular_auditor.py
+++ week 3/modular_auditor.py
@@ -16,8 +16,12 @@ def get_valid_inputs():
             print("Error: Invalid entry. Please enter a valid number.")
             failed_entries += 1
 
+def process_delivery(current_total, new_value):
+    return current_total + new_value
+
+
 inventory = 0
-failed_entries = 0  # Requirement 8: Track rejected inputs
+failed_entries = 0 
 
 while True:
     val = get_valid_inputs()
@@ -25,7 +29,7 @@ while True:
     if val == "quit":
         break
 
-    inventory += val
+    inventory = process_delivery(inventory, val)
 
     if inventory > 500:
         print("OVERSTOCK ALERT: Total inventory of " + str(inventory) + " exceeds limit of 500 units!")
