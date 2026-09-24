dff06e599c32be3f2a9e4acdc2b5cbbebc375b6f
Johanan 2603048 <2603048@sit.singaporetech.edu.sg>
2026-09-25 00:00:44 +0800

    added week 3 dockerfile and log.txt as well as first commit of week 4

---
+++ week 4/persistent_auditor.py
@@ -0,0 +1,49 @@
+def get_valid_inputs():
+    global failed_entries
+    while True:
+        userInput = input("Enter a stock quantity: (or type 'quit' to quit)")
+        if userInput == "quit":
+            return "quit"
+        
+        elif userInput.startswith("-") and userInput[1:].isdigit():
+            print("Error: Negative numbers are not allowed.")
+            failed_entries += 1
+
+        elif userInput.isdigit():
+            return int(userInput)
+        
+        else:
+            print("Error: Invalid entry. Please enter a valid number.")
+            failed_entries += 1
+
+def process_delivery(current_total, new_value):
+    return current_total + new_value
+
+def calculate_tax(amount):
+    return amount * 0.10
+
+def generate_report(total_units, failed_attempts):
+    print("Total Deliveries Processed: " + str(total_units))
+    print("Number of Failed/Rejected Entries: " + str(failed_attempts))
+
+
+
+inventory = 0
+failed_entries = 0 
+
+while True:
+    val = get_valid_inputs()
+
+    if val == "quit":
+        break
+
+    tax = calculate_tax(val)
+    print("Tax for this delivery (10%): " + str(tax))
+     
+    inventory = process_delivery(inventory, val)
+
+    if inventory > 500:
+        print("OVERSTOCK ALERT: Total inventory of " + str(inventory) + " exceeds limit of 500 units!")
+        break
+
+generate_report(inventory, failed_entries)
